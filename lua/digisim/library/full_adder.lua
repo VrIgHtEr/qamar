60ea194dce141abb2d4c03987d167c378add39f0
Cedric Mamo <vrighter@gmail.com>
2022-05-10 19:29:56 +0200

    digisim

--- lua/digisim/library/full_adder.lua
+++ lua/digisim/library/full_adder.lua
@@ -13,31 +13,21 @@ return function(simulation)
 			opts.names = { inputs = { "a", "b", "c" }, outputs = { "sum", "carry" } }
 			circuit:add_component(adder, 3, 2, nil, opts)
 
-			local xa = adder .. ".xa"
-			local xb = adder .. ".xb"
-			local aa = adder .. ".aa"
-			local ab = adder .. ".ab"
+			local ha = adder .. ".ha"
+			local hb = adder .. ".hb"
 			local o = adder .. ".o"
 
 			circuit
-				:new_xor(xa)
-				:c(adder, "a", xa, "a")
-				:c(adder, "b", xa, "b")
-				:new_xor(xb)
-				:c(xa, "q", xb, "a")
-				:c(adder, "c", xb, "b")
-				:c(xb, "q", adder, "sum")
-
-			circuit
-				:new_and(aa)
-				:c(xa, "q", aa, "a")
-				:c(adder, "c", aa, "b")
-				:new_and(ab)
-				:c(adder, "a", ab, "a")
-				:c(adder, "b", ab, "b")
+				:new_half_adder(ha)
+				:c(adder, "a", ha, "a")
+				:c(adder, "b", ha, "b")
+				:new_half_adder(hb)
+				:c(adder, "c", hb, "a")
+				:c(ha, "sum", hb, "b")
+				:c(hb, "sum", adder, "sum")
 				:new_or(o)
-				:c(aa, "q", o, "a")
-				:c(ab, "q", o, "b")
+				:c(ha, "carry", o, "a")
+				:c(hb, "carry", o, "b")
 				:c(o, "q", adder, "carry")
 		end
 	)
