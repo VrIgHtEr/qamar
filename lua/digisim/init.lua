f3e0fa232119a8100c8e8161e3faaacdc4111b4a
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-05-11 14:42:43 +0200

    added mux implementation

--- lua/digisim/init.lua
+++ lua/digisim/init.lua
@@ -2,64 +2,13 @@ local constants = require("digisim.constants")
 local signal = require("digisim.signal")
 local simulation = require("digisim.simulation")
 
-function print(circuit)
-	do
-		io.stdout:write(tostring(circuit.trace))
-		io.stdout:write("\n")
-		return
-	end
-	local max = 0
-	local maxtime = 0
-	for k, t in pairs(circuit.trace) do
-		if k:sub(1, 1) ~= "[" then
-			max = math.max(max, k:len())
-			maxtime = math.max(maxtime, t[#t].time)
-		end
-	end
-	maxtime = maxtime + constants.CLOCK_PERIOD_TICKS / 4
-	max = max + 2
-	local traces = {}
-	for k in pairs(circuit.trace) do
-		if k:sub(1, 1) ~= "[" then
-			table.insert(
-				traces,
-				table.concat({ k, ": ", string.rep(" ", max - (k:len() + 2)), circuit:get_trace(k, maxtime) })
-			)
-		end
-	end
-	max = 0
-	for _, x in ipairs(traces) do
-		max = math.max(max, x:len())
-	end
-	max = max + 5
-
-	table.sort(traces)
-	local first = true
-	local lines = {}
-	for _, x in ipairs(traces) do
-		if first then
-			first = false
-		else
-			table.insert(lines, "--")
-			--			print("--")
-		end
-		table.insert(lines, "--" .. x)
-		--		print(x)
-	end
-
-	table.insert(lines, "")
-	io.stdout:write(table.concat(lines, "\n"))
-	--	vim.api.nvim_buf_set_text(vim.api.nvim_get_current_buf(), -1, 0, -1, 0, lines)
-end
-
 do
-	--vim.api.nvim_exec("mes clear", true)
-
 	local circuit = simulation.new()
+
+	circuit:new_clock("CLK", { period = constants.CLOCK_PERIOD_TICKS, trace = true })
 
 	circuit:new_clock("DATA", { period = constants.CLOCK_PERIOD_TICKS * 2, trace = true })
 	circuit:new_reset("RST_", { period = constants.STARTUP_TICKS, trace = true })
-	circuit:new_clock("CLK", { period = constants.CLOCK_PERIOD_TICKS, trace = true })
 	circuit:new_edge_detector("ECLK", { chain_length = 4, trace = true }):c("CLK", "q", "ECLK", "clk")
 	circuit
 		:new_ms_jk_flipflop("FF", { trace = true })
@@ -70,7 +19,8 @@ do
 	circuit:c("DATA", "q", "FF", "j")
 	circuit:c("ND", "q", "FF", "k")
 
-	circuit:new_n_bit_adder("adder", { width = 8, trace = true })
+	local adder_width = 32
+	circuit:new_n_bit_adder("adder", { width = adder_width, trace = true })
 	local function create_random_input(name)
 		local value = signal.low
 		local last = 0
@@ -85,22 +35,10 @@ do
 			end, { trace = true, names = { outputs = { "q" } } })
 			:c(name, "q", "adder", name)
 	end
-	create_random_input("a0")
-	create_random_input("a1")
-	create_random_input("a2")
-	create_random_input("a3")
-	create_random_input("a4")
-	create_random_input("a5")
-	create_random_input("a6")
-	create_random_input("a7")
-	create_random_input("b0")
-	create_random_input("b1")
-	create_random_input("b2")
-	create_random_input("b3")
-	create_random_input("b4")
-	create_random_input("b5")
-	create_random_input("b6")
-	create_random_input("b7")
+	for i = 0, adder_width - 1 do
+		create_random_input("a" .. i)
+		create_random_input("b" .. i)
+	end
 
 	circuit:new_clock("C", { period = constants.CLOCK_PERIOD_TICKS, trace = true }):c("C", "q", "adder", "cin")
 
---
+++ lua/digisim/library/mux.lua
@@ -0,0 +1,71 @@
+---@class simulation
+---@field new_mux function
+
+---@param simulation simulation
+return function(simulation)
+	simulation:register_component(
+		"mux",
+		---@param circuit simulation
+		---@param name string
+		---@param opts boolean
+		function(circuit, name, opts)
+			opts = opts or { width = 1 }
+			local width = opts.width or 1
+			if type(width) ~= "number" or width < 1 or width > 16 then
+				error("invalid width type")
+			end
+			width = math.floor(width)
+			opts.names = { inputs = {}, outputs = { "q" } }
+			for i = 1, width do
+				opts.names.inputs[i] = "a" .. (i - 1)
+			end
+			local datapins = math.pow(2, width)
+			for i = 1, datapins do
+				opts.names.inputs[width + i] = "i" .. (i - 1)
+			end
+
+			local zero = name .. ".a"
+			local one = name .. ".b"
+			local inv = name .. ".n"
+
+			if width == 1 then
+				circuit
+					:add_component(name, nil, opts)
+					:new_and(zero)
+					:new_and(one)
+					:c(zero, "q", one, "q")
+					:c(one, "q", name, "q")
+					:new_not(inv)
+					:c(inv, "q", zero, "a")
+					:c(name, "a0", inv, "a")
+					:c(name, "a0", one, "a")
+					:c(name, "i0", zero, "b")
+					:c(name, "i1", one, "b")
+			else
+				circuit:new_mux(zero, { width = width - 1 })
+				circuit:new_mux(one, { width = width - 1 })
+				for i = 1, width - 1 do
+					circuit:c(name, "a" .. (i - 1), zero, "a" .. (i - 1))
+					circuit:c(name, "a" .. (i - 1), one, "a" .. (i - 1))
+				end
+				for i = 1, datapins * 0.5 do
+					circuit:c(name, "i" .. (i - 1), zero, "i" .. (i - 1))
+					circuit:c(name, "i" .. (i + (datapins * 0.5) - 1), one, "i" .. (i - 1))
+				end
+				local az = name .. ".az"
+				local ao = name .. ".ao"
+				circuit
+					:new_and(az)
+					:c(zero, "q", az, "a")
+					:new_and(ao)
+					:c(one, "q", ao, "a")
+					:new_not(inv)
+					:c(name, "a" .. (width - 1), inv, "a")
+					:c(inv, "q", az, "b")
+					:c(name, "a" .. (width - 1), ao, "b")
+					:c(az, "q", ao, "q")
+					:c(ao, "q", name, "q")
+			end
+		end
+	)
+end
