9bfbba7f761b069fc2685a514dd502f96d1a6d9b
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-05-12 14:13:24 +0200

    made xor and xnor variable width

--- lua/digisim/library/xor.lua
+++ lua/digisim/library/xor.lua
@@ -11,11 +11,24 @@ return function(simulation)
 		---@param name string
 		---@param opts boolean
 		function(self, name, opts)
-			opts = opts or {}
-			opts.names = { inputs = { "a", "b" }, outputs = { "q" } }
-			return self:add_component(name, function(_, a, b)
-				return (a == signal.high and b == signal.low or a == signal.low and b == signal.high) and signal.high
-					or signal.low
+			opts = opts or { width = 2 }
+			local width = opts.width or 2
+			if type(width) ~= "number" then
+				error("invalid width type")
+			end
+			width = math.floor(width)
+			if width < 2 then
+				error("invalid width")
+			end
+			opts.names = { inputs = { { "in", width } }, outputs = { "q" } }
+			return self:add_component(name, function(_, a)
+				local ret = false
+				for _, x in ipairs(a) do
+					if x == signal.high then
+						ret = not ret
+					end
+				end
+				return ret and signal.high or signal.low
 			end, opts)
 		end
 	)
