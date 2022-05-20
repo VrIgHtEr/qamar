ddb4d88f1ccdd6e75c97336d2c6fdcbc1c934877
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-05-20 14:37:08 +0200

    digisim

--- lua/digisim/library/instruction_decoder.lua
+++ lua/digisim/library/instruction_decoder.lua
@@ -105,19 +105,19 @@ return function(simulation)
 			local J = n .. "J"
 			self:new_and(J):cp(1, dec, "in", 4, J, "in", 1):cp(1, dec, "in", 3, J, "in", 2)
 			local F = n .. "F"
-			self:new_and(F):cp(1, nec, "a", 14, F, "in", 1):cp(1, nec, "a", 15, F, "in", 2)
+			self:new_and(F):cp(1, nec, "q", 14, F, "in", 1):cp(1, nec, "q", 15, F, "in", 2)
 			local B = n .. "B"
 			self
 				:new_and(B, { width = 3 })
-				:cp(1, nec, "a", 28, B, "in", 1)
-				:cp(1, nec, "a", 27, B, "in", 2)
-				:cp(1, nec, "a", 26, B, "in", 3)
+				:cp(1, nec, "q", 28, B, "in", 1)
+				:cp(1, nec, "q", 27, B, "in", 2)
+				:cp(1, nec, "q", 26, B, "in", 3)
 			local C = n .. "C"
 			self
 				:new_and(C, { width = 3 })
-				:cp(1, nec, "a", 32, C, "in", 1)
-				:cp(1, nec, "a", 30, C, "in", 2)
-				:cp(1, nec, "a", 29, C, "in", 3)
+				:cp(1, nec, "q", 32, C, "in", 1)
+				:cp(1, nec, "q", 30, C, "in", 2)
+				:cp(1, nec, "q", 29, C, "in", 3)
 			local E = n .. "E"
 			self:new_and(E):cp(1, B, "q", 1, E, "in", 1):cp(1, C, "q", 1, E, "in", 2)
 			local S = n .. "S"
@@ -132,7 +132,7 @@ return function(simulation)
 			self:new_and(T):cp(1, H, "q", 1, T, "in", 1):cp(1, nec, "q", 5, T, "in", 2)
 			local V = n .. "V"
 			self:new_and(V):cp(1, T, "q", 1, V, "in", 1):cp(1, J, "q", 1, V, "in", 2)
-
+			self:c(V, "q", dec, "j")
 			local D = n .. "D"
 			self
 				:new_and(D, { width = 3 })
@@ -146,6 +146,59 @@ return function(simulation)
 				:cp(1, D, "q", 1, G, "in", 1)
 				:cp(1, dec, "in", 3, G, "in", 2)
 				:cp(1, dec, "in", 5, G, "in", 3)
+			self:c(G, "q", dec, "u")
+
+			local I = n .. "I"
+			self:new_and(I):cp(1, nec, "q", 3, I, "in", 1):cp(1, nec, "q", 4, I, "in", 2)
+			local P = n .. "P"
+			self:new_and(P):cp(1, H, "q", 1, P, "in", 1):cp(1, I, "q", 1, P, "in", 2)
+			local R = n .. "R"
+			self:new_and(R):cp(1, P, "q", 1, R, "in", 1):cp(1, nec, "q", 5, R, "in", 2)
+			local U = n .. "U"
+			self:new_and(U):cp(1, R, "q", 1, U, "in", 1):cp(1, S, "q", 1, U, "in", 2)
+			local B1 = n .. "B1"
+			self:new_and(B1):cp(1, U, "q", 1, B1, "in", 1):cp(1, dec, "in", 15, B1, "in", 2)
+			local B2 = n .. "B2"
+			self:new_and(B2):cp(1, U, "q", 1, B2, "in", 1):cp(1, nec, "q", 14, B2, "in", 2)
+			local B3 = n .. "B3"
+			self:new_or(B3):cp(1, B1, "q", 1, B3, "in", 1):cp(1, B2, "q", 1, B3, "in", 2)
+			self:c(B3, "q", dec, "b")
+			local W = n .. "W"
+			self:new_and(W):cp(1, E, "q", 1, W, "in", 1):cp(1, dec, "in", 5, W, "in", 2)
+			local Q = n .. "Q"
+			self:new_and(Q):cp(1, P, "q", 1, Q, "in", 1):cp(1, dec, "in", 5, Q, "in", 2)
+			local L = n .. "L"
+			self:new_and(L):cp(1, D, "q", 1, L, "in", 1):cp(1, nec, "q", 3, L, "in", 2)
+			local M = n .. "M"
+			self:new_and(M):cp(1, L, "q", 1, M, "in", 1):cp(1, nec, "q", 6, M, "in", 2)
+			local N = n .. "N"
+			self:new_and(N):cp(1, L, "q", 1, N, "in", 1):cp(1, dec, "in", 6, N, "in", 2)
+			local O = n .. "O"
+			self:new_and(O):cp(1, N, "q", 1, O, "in", 1):cp(1, dec, "in", 5, O, "in", 2)
+			local K = n .. "K"
+			self
+				:new_and(K, { width = 5 })
+				:cp(1, S, "q", 1, K, "in", 1)
+				:cp(1, J, "q", 1, K, "in", 2)
+				:cp(1, nec, "q", 5, K, "in", 3)
+				:cp(1, nec, "q", 6, K, "in", 4)
+				:cp(1, nec, "q", 7, K, "in", 5)
+			local S1 = n .. "S1"
+			self
+				:new_and(S1, { width = 3 })
+				:cp(1, N, "q", 1, S1, "in", 1)
+				:cp(1, F, "q", 1, S1, "in", 2)
+				:cp(1, nec, "q", 5, S1, "in", 3)
+			local S2 = n .. "S2"
+			self
+				:new_and(S2, { width = 4 })
+				:cp(1, N, "q", 1, S2, "in", 1)
+				:cp(1, nec, "q", 13, S2, "in", 3)
+				:cp(1, nec, "q", 15, S2, "in", 3)
+				:cp(1, nec, "q", 5, S2, "in", 3)
+			local S3 = n .. "S3"
+			self:new_or(S3):cp(1, S1, "q", 1, S3, "in", 1):cp(1, S2, "q", 1, S3, "in", 2)
+			self:c(S3, "q", dec, "s")
 
 			--[[
 A = b1b2
@@ -160,18 +213,17 @@ T = H !b5
 V = T J
 D = S!b7!b4
 G = b3 D b5
-
 I = !b4 !b3
-K = S !b7 !b6 J !b5
+P = H I
+R = !b5 P
+U = R S
+W = b5 E
+Q = b5 P
 L = !b3 D
 M = !b6 L
 N = b6 L
 O = b5 M
-P = H I
-Q = b5 P
-R = !b5 P
-U = R S
-W = b5 E
+K = S !b7 !b6 J !b5
 
 ========================================
              BBBB
@@ -193,17 +245,11 @@ VKS7NOM6LJ43D5431CBEFGH5WPQRTSU   RISBUJ
 ---------------1----------1---- | .1....
 --------------1-----------1---- | .1....
 ----------01---0----1-------1-- | .1....
+
 ----1---------------1--0------- | ..1...
 ----1--------0-0-------0------- | ..1...
--------------1----------------1 | ...1..
---------------0---------------1 | ...1..
-
----------------------1--------- | ....1.
-1------------------------------ | .....1
 
 ]]
-			self:c(G, "q", dec, "u")
-			self:c(V, "q", dec, "j")
 			return self
 		end
 	)
