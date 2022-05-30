a7dab386aee6aea69b369464dbd97017018bcdcf
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-05-30 13:31:41 +0200

    digisim

--- test.asm
+++ test.asm
@@ -1,3 +1,65 @@
+addi x2, x0, 1
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
