19c4d9f6e91fe609791194096514380cd40ade10
Cedric Mamo <vrighter@gmail.com>
2022-05-31 00:29:15 +0200

    digisim

--- test.asm
+++ test.asm
@@ -4,78 +4,222 @@ add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
 
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
 
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
 
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
 
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
 
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
 
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
 
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
 
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
 
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
 
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
 
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
 
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
 
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
 
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
 
 add x3, x2, x1
 add x1, x0, x2
 add x2, x0, x3
 andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
+
+add x3, x2, x1
+add x1, x0, x2
+add x2, x0, x3
+andi x2, x2, 255
+slli x2, x2, 24
+srai x2, x2, 24
