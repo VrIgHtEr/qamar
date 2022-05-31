01a7323a7ac81d7cfd443839fac049ff2309def8
Cedric Mamo <vrighter@gmail.com>
2022-05-31 23:06:07 +0200

    Revert "Revert "digisim""
    This reverts commit 2f7114728d16e5cd2c085a3c0a072e316237d8da.

--- lua/digisim/library/alu.lua
+++ lua/digisim/library/alu.lua
@@ -30,9 +30,7 @@ return function(simulation)
 				outputs = {
 					{ "out", width },
 					"zero",
-					"sign",
-					"signa",
-					"signb",
+					"lt",
 				},
 			}
 
@@ -52,8 +50,6 @@ return function(simulation)
 				sim:cp(width, alu, "out", 1, zero, "in", 1)
 			end
 			sim:c(zero, "q", alu, "zero")
-			sim:cp(1, alu, "a", width, alu, "signa", 1)
-			sim:cp(1, alu, "b", width, alu, "signb", 1)
 
 			--output tristate buffer
 			sim:new_tristate_buffer(out, { width = width })
@@ -63,7 +59,6 @@ return function(simulation)
 			sim:new_mux_bank(lm, { width = 3, bits = width })
 			sim:c(alu, "sel", lm, "sel")
 			sim:c(lm, "out", out, "a")
-			sim:cp(1, lm, "d0", width, alu, "sign", 1)
 
 			--- conditional inverter for input B
 			sim:new_xor_bank(xb, { width = width })
@@ -120,7 +115,21 @@ return function(simulation)
 			sim:cp(width - 1, alu, "a", 1, comparator, "a", 1)
 			sim:cp(width - 1, alu, "b", 1, comparator, "b", 1)
 
-			return sim
+			local s = alu .. ".signed"
+			sim:new_and_bank(s):c(alu, "u", s, "a")
+
+			local cx = alu .. ".cx"
+			sim
+				:new_xor_bank(cx, { width = 2 })
+				:cp(1, s, "q", 1, cx, "a", 1)
+				:cp(1, s, "q", 1, cx, "a", 2)
+				:cp(1, alu, "a", width, cx, "b", 1)
+				:cp(1, alu, "b", width, cx, "b", 2)
+				:cp(1, cx, "q", 1, comparator, "a", width)
+				:cp(1, cx, "q", 2, comparator, "b", width)
+
+			local lt = alu .. ".lt"
+			sim:new_not(lt):c(comparator, "q", lt, "a"):c(lt, "q", alu, "lt")
 		end
 	)
 end
