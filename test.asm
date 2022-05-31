00ace5e99a990235b631ab0e2efef49b23b41b89
Cedric Mamo <vrighter@gmail.com>
2022-06-01 00:55:51 +0200

    digisim

--- test.asm
+++ test.asm
@@ -19,23 +19,3 @@ mul_end:
 xor x5,x5,x5
 xor x6,x6,x6
 
-xor x1, x1, x1
-addi x2, x0, 1
-addi x4, x0, 24
-
-loop:
-add x3, x1, x2
-add x1, x2, x0
-add x2, x3, x0
-andi x2, x2, 255
-sll x2, x2, x4
-sra x2, x2, x4
-
-add x3, x2, x1
-add x1, x2, x0
-add x2, x3, x0
-andi x2, x2, 255
-slli x2, x2, 24
-srai x2, x2, 24
-
-beq s0, s0, loop
