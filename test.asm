132cfe8c14dfbc1bb48bfb7585df3a137dbb531e
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-06-01 15:11:38 +0200

    digisim

--- test.asm
+++ test.asm
@@ -2,18 +2,18 @@ li x1, 1000000000
 addi a0,zero, 9
 addi a1,zero, 42
 mul:
-add t0, a0, zero
+mv t0, a0
 xor a0, a0, a0
 xor t1, t1, t1
 mul_loop:
-beq a1, zero, mul_end
+beqz a1, mul_end
 andi t2, a1, 1
-beq t2, zero, mul_continue
+beqz t2, mul_continue
 sll t2, t0, t1
 add a0, a0, t2
 mul_continue:
 srli a1, a1, 1
 addi t1, t1, 1
-beq zero, zero, mul_loop
+beqz zero, mul_loop
 mul_end:
 
