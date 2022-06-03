f3bce14b776bd0fdb56c79a54a9c06af4044f347
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-06-03 10:17:32 +0200

    digisim

--- lua/digisim/constants.lua
+++ lua/digisim/constants.lua
@@ -1,6 +1,6 @@
 return {
 	SIM_TIME = 262144,
-	DEBUG_TRACE_ALL_OUTPUTS = false,
+	DEBUG_TRACE_ALL_OUTPUTS = true,
 	TRACE_INPUTS = true,
 	STARTUP_TICKS = 12,
 	NAND_ONLY = false,
--- test.asm
+++ test.asm
@@ -1,19 +1,27 @@
-li x1, 1000000000
-addi a0,zero, 9
-addi a1,zero, 42
-mul:
-mv t0, a0
-li a0,0
-li t1,0
-mul_loop:
-beqz a1, mul_end
-andi t2, a1, 1
-beqz t2, mul_continue
-sll t2, t0, t1
-add a0, a0, t2
-mul_continue:
-srli a1, a1, 1
-addi t1, t1, 1
-beqz zero, mul_loop
-mul_end:
+.equ val, 7
+li x1, 10
+li x2, val
+slti x3, x1, val
+slt x3, x1, x2
+sltiu x3, x1, val
+sltu x3, x1, x2
 
+#li x1, 1000000000
+#addi a0,zero, 9
+#addi a1,zero, 42
+#mul:
+#mv t0, a0
+#li a0,0
+#li t1,0
+#mul_loop:
+#beqz a1, mul_end
+#andi t2, a1, 1
+#beqz t2, mul_continue
+#sll t2, t0, t1
+#add a0, a0, t2
+#mul_continue:
+#srli a1, a1, 1
+#addi t1, t1, 1
+#beqz zero, mul_loop
+#mul_end:
+#
