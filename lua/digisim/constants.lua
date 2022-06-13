c2d27f67b9f6f981ad458f28956ad2bb0d03410b
Cedric Mamo <vrighter@gmail.com>
2022-06-13 17:51:41 +0200

    digisim

--- lua/digisim/constants.lua
+++ lua/digisim/constants.lua
@@ -1,6 +1,6 @@
 return {
-	SIM_TIME = 1048576 * 10,
-	DEBUG_TRACE_ALL_OUTPUTS = false,
+	SIM_TIME = 1048576 * 2,
+	DEBUG_TRACE_ALL_OUTPUTS = true,
 	TRACE_INPUTS = true,
 	NAND_ONLY = false,
 }
--- lua/digisim/library/instruction_store.lua
+++ lua/digisim/library/instruction_store.lua
@@ -21,6 +21,7 @@ return function(simulation)
 					{ "opcode", 7 },
 					{ "funct3", 3 },
 					{ "d", WIDTH },
+					{ "sram_out", 8 },
 				},
 				outputs = {
 					"icomplete",
@@ -33,6 +34,7 @@ return function(simulation)
 					"sram_write",
 					{ "sram_address", WIDTH },
 					{ "sram_in", 8 },
+					{ "TEST_D", 32 },
 				},
 			}
 			s:add_component(f, opts)
@@ -46,12 +48,18 @@ return function(simulation)
 
 			local a = f .. ".aliases"
 			s
-				:add_component(a, { names = { inputs = {
-					"store",
-					"load",
-				} } })
+				:add_component(a, {
+					names = { inputs = {
+						"store",
+						"load",
+						"signed",
+						{ "out", WIDTH },
+					} },
+				})
 				:cp(1, f, "opcode", 6, a, "store", 1)
 				:cp(1, nopcode, "q", 4, a, "load", 1)
+				:cp(1, nf3, "q", 3, a, "signed", 1)
+				:c(a, "out", f, "TEST_D")
 
 			local valid_load1 = f .. ".vload1"
 			s:new_and(valid_load1):cp(1, nf3, "q", 2, valid_load1, "in", 1):cp(1, f, "funct3", 3, valid_load1, "in", 2)
@@ -75,15 +83,15 @@ return function(simulation)
 			s:new_or_bank(valid):c(valid_load, "q", valid, "a"):c(valid_store, "q", valid, "b")
 
 			local legal = f .. ".legal"
-			s:new_and(legal, { width = 9 })
+			s:new_and(legal, { width = 8 })
 			s:cp(2, f, "opcode", 1, legal, "in", 1)
 			s:cp(3, nopcode, "q", 1, legal, "in", 3)
 			s:cp(1, nopcode, "q", 5, legal, "in", 6)
 			s:cp(1, valid, "q", 1, legal, "in", 7)
 			s:cp(1, vnf3, "q", 1, legal, "in", 8)
 
-			-- only allow writes for now
-			s:cp(1, a, "store", 1, legal, "in", 9)
+			-- only allow writes
+			--s:cp(1, a, "store", 1, legal, "in", 9)
 
 			local legalbuf = f .. ".legalbuf"
 			s
@@ -200,20 +208,6 @@ return function(simulation)
 			s:c(f, "falling", stage4, "falling")
 			s:c(stage3, "q", stage4, "d")
 
-			local writing1 = f .. ".writing1"
-			s
-				:new_or(writing1, { width = 4 })
-				:cp(1, stage1, "q", 1, writing1, "in", 1)
-				:cp(1, stage2, "q", 1, writing1, "in", 2)
-				:cp(1, stage3, "q", 1, writing1, "in", 3)
-				:cp(1, stage4, "q", 1, writing1, "in", 4)
-			local writing = f .. ".writing"
-			s
-				:new_and(writing)
-				:cp(1, writing1, "q", 1, writing, "in", 1)
-				:cp(1, a, "store", 1, writing, "in", 2)
-				:c(writing, "q", addr_buf, "en")
-
 			local comp1 = f .. ".c1"
 			s:new_and_bank(comp1):c(stage1, "q", comp1, "a"):c(nstage2en, "q", comp1, "b")
 			local comp2 = f .. ".c2"
@@ -236,6 +230,17 @@ return function(simulation)
 			s:new_tristate_buffer(icomplete):c(stagec, "q", icomplete, "en"):high(icomplete, "a")
 			s:c(icomplete, "q", f, "icomplete")
 
+			local active = f .. ".active"
+			s
+				:new_or(active, { width = 4 })
+				:cp(1, stage1, "q", 1, active, "in", 1)
+				:cp(1, stage2, "q", 1, active, "in", 2)
+				:cp(1, stage3, "q", 1, active, "in", 3)
+				:cp(1, stage4, "q", 1, active, "in", 4)
+				:c(active, "q", addr_buf, "en")
+			local writing = f .. ".writing"
+			s:new_and(writing):cp(1, active, "q", 1, writing, "in", 1):cp(1, a, "store", 1, writing, "in", 2)
+
 			local write = f .. ".write"
 			s
 				:new_tristate_buffer(write, { width = 3 })
@@ -246,30 +251,119 @@ return function(simulation)
 				:cp(1, write, "q", 2, f, "rs2", 1)
 				:cp(1, write, "q", 3, f, "alu_oe", 1)
 
+			local stage1w = f .. "stage1w"
+			s:new_and_bank(stage1w):c(stage1, "q", stage1w, "a"):c(writing, "q", stage1w, "b")
 			local b0 = f .. ".b0"
 			s
 				:new_tristate_buffer(b0, { width = 8 })
-				:c(stage1, "q", b0, "en")
+				:c(stage1w, "q", b0, "en")
 				:cp(8, f, "d", 1, b0, "a", 1)
 				:cp(8, b0, "q", 1, f, "sram_in", 1)
+			local stage2w = f .. "stage2w"
+			s:new_and_bank(stage2w):c(stage2, "q", stage2w, "a"):c(writing, "q", stage2w, "b")
 			local b1 = f .. ".b1"
 			s
 				:new_tristate_buffer(b1, { width = 8 })
-				:c(stage2, "q", b1, "en")
+				:c(stage2w, "q", b1, "en")
 				:cp(8, f, "d", 9, b1, "a", 1)
 				:cp(8, b1, "q", 1, f, "sram_in", 1)
+			local stage3w = f .. "stage3w"
+			s:new_and_bank(stage3w):c(stage3, "q", stage3w, "a"):c(writing, "q", stage3w, "b")
 			local b2 = f .. ".b2"
 			s
 				:new_tristate_buffer(b2, { width = 8 })
-				:c(stage3, "q", b2, "en")
+				:c(stage3w, "q", b2, "en")
 				:cp(8, f, "d", 17, b2, "a", 1)
 				:cp(8, b2, "q", 1, f, "sram_in", 1)
+			local stage4w = f .. "stage4w"
+			s:new_and_bank(stage4w):c(stage4, "q", stage4w, "a"):c(writing, "q", stage4w, "b")
 			local b3 = f .. ".b3"
 			s
 				:new_tristate_buffer(b3, { width = 8 })
-				:c(stage4, "q", b3, "en")
+				:c(stage4w, "q", b3, "en")
 				:cp(8, f, "d", 25, b3, "a", 1)
 				:cp(8, b3, "q", 1, f, "sram_in", 1)
+
+			local reading = f .. ".reading"
+			s:new_and(reading):cp(1, active, "q", 1, reading, "in", 1):cp(1, a, "load", 1, reading, "in", 2)
+
+			local stage1r = f .. "stage1r"
+			s
+				:new_and(stage1r, { width = 3 })
+				:cp(1, stage1, "q", 1, stage1r, "in", 1)
+				:cp(1, reading, "q", 1, stage1r, "in", 2)
+				:cp(1, f, "rising", 1, stage1r, "in", 3)
+			local in0 = f .. ".in0"
+			s
+				:new_ms_d_flipflop_bank(in0, { width = 8 })
+				:c(f, "rst~", in0, "rst~")
+				:c(f, "falling", in0, "falling")
+				:c(stage1r, "q", in0, "rising")
+				:c(f, "sram_out", in0, "d")
+
+			local stage2r = f .. "stage2r"
+			s
+				:new_and(stage2r, { width = 3 })
+				:cp(1, stage2, "q", 1, stage2r, "in", 1)
+				:cp(1, reading, "q", 1, stage2r, "in", 2)
+				:cp(1, f, "rising", 1, stage2r, "in", 3)
+			local in1 = f .. ".in1"
+			s
+				:new_ms_d_flipflop_bank(in1, { width = 8 })
+				:c(f, "rst~", in1, "rst~")
+				:c(f, "falling", in1, "falling")
+				:c(stage2r, "q", in1, "rising")
+				:c(f, "sram_out", in1, "d")
+
+			local stage3r = f .. "stage3r"
+			s
+				:new_and(stage3r, { width = 3 })
+				:cp(1, stage3, "q", 1, stage3r, "in", 1)
+				:cp(1, reading, "q", 1, stage3r, "in", 2)
+				:cp(1, f, "rising", 1, stage3r, "in", 3)
+			local in2 = f .. ".in2"
+			s
+				:new_ms_d_flipflop_bank(in2, { width = 8 })
+				:c(f, "rst~", in2, "rst~")
+				:c(f, "falling", in2, "falling")
+				:c(stage3r, "q", in2, "rising")
+				:c(f, "sram_out", in2, "d")
+
+			local stage4r = f .. "stage4r"
+			s
+				:new_and(stage4r, { width = 3 })
+				:cp(1, stage4, "q", 1, stage4r, "in", 1)
+				:cp(1, reading, "q", 1, stage4r, "in", 2)
+				:cp(1, f, "rising", 1, stage4r, "in", 3)
+			local in3 = f .. ".in3"
+			s
+				:new_ms_d_flipflop_bank(in3, { width = 8 })
+				:c(f, "rst~", in3, "rst~")
+				:c(f, "falling", in3, "falling")
+				:c(stage4r, "q", in3, "rising")
+				:c(f, "sram_out", in3, "d")
+
+			local sext1 = f .. ".sext1"
+			s:new_and(sext1):cp(1, a, "signed", 1, sext1, "in", 1):cp(1, in0, "q", 7, sext1, "in", 2)
+
+			local out1 = f .. ".out1"
+			s:new_mux_bank(out1, { bits = 8 })
+			s:c(in0, "q", out1, "d0")
+			s:fanout(sext1, "q", 1, out1, "d1", 1, 8)
+			s:c(is_single_byte, "q", out1, "sel")
+
+			local sext23 = f .. ".sext23"
+			s:new_and(sext23):cp(1, a, "signed", 1, sext23, "in", 1):cp(1, out1, "out", 7, sext23, "in", 2)
+			local out23 = f .. ".out23"
+			s:new_mux_bank(out23, { bits = 16 })
+			s:cp(8, in2, "q", 1, out23, "d0", 1)
+			s:cp(8, in3, "q", 1, out23, "d0", 9)
+			s:fanout(sext23, "q", 1, out23, "d1", 1, 16)
+			s:cp(1, nf3, "q", 2, out23, "sel", 1)
+
+			s:cp(8, in0, "q", 1, a, "out", 1)
+			s:cp(8, out1, "out", 1, a, "out", 9)
+			s:cp(16, out23, "out", 1, a, "out", 17)
 		end
 	)
 end
