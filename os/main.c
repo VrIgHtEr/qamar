218c80134cecf81b5a64dd7e30ab7bbd0bdba00f
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-06-15 14:45:15 +0200

    digisim

--- os/main.c
+++ os/main.c
@@ -42,7 +42,7 @@ int solve_grid(grid_t *grid, int level) {
       // iterate over all columns
       for (j = 0; j < 9; j++) {
         int box, sol, solcount;
-        int v[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int8_t v[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         // skip slots that already have an assigned value
         if (g.grid[9 * i + j] != 0) {
