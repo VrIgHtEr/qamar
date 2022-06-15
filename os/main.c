8998dfc5e014e50540502a6f0169ffe68b5ece22
Cedric Mamo <vrighter@gmail.com>
2022-06-15 22:41:58 +0200

    digisim

--- os/main.c
+++ os/main.c
@@ -14,16 +14,11 @@ static void printgrid(const int8_t *grid) {
 void *memcpy(void *dest, const void *src, size_t size) {
   uint8_t *pdest = (uint8_t *)dest;
   uint8_t *psrc = (uint8_t *)src;
-  for (size_t loops = (size / sizeof(uint32_t)); loops; --loops) {
+  for (size_t loops = (size / sizeof(uint32_t)); loops;
+       --loops, pdest += sizeof(uint32_t), psrc += sizeof(uint32_t))
     *((uint32_t *)pdest) = *((uint32_t *)psrc);
-    pdest += sizeof(uint32_t);
-    psrc += sizeof(uint32_t);
-  }
-  for (size_t loops = (size % sizeof(uint32_t)); loops; --loops) {
-    *pdest = *psrc;
-    ++pdest;
-    ++psrc;
-  }
+  for (size_t loops = (size % sizeof(uint32_t)); loops; --loops)
+    *(pdest++) = *(psrc++);
   return dest;
 }
 
