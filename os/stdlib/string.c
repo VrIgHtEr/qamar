e095f2e350a07d274949b27ff1fa840a7da4e426
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-06-16 09:39:35 +0200

    digisim

--- os/stdlib/string.c
+++ os/stdlib/string.c
@@ -11,3 +11,18 @@ void *memcpy(void *dest, const void *src, size_t size) {
     *(pdest++) = *(psrc++);
   return dest;
 }
+
+void *memset(void *dest, int32_t val, size_t size) {
+  uint8_t *pdest = (uint8_t *)dest;
+  if (size >= 4) {
+    val = val & 0xFF;
+    val = val | (val << 8);
+    val = val | (val << 16);
+    for (size_t loops = (size / sizeof(uint32_t)); loops;
+         --loops, pdest += sizeof(uint32_t))
+      *((uint32_t *)pdest) = val;
+  }
+  for (size_t loops = (size % sizeof(uint32_t)); loops; --loops)
+    *(pdest++) = (uint8_t)val;
+  return dest;
+}
