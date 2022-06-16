00609e0557e174d22dfebca06915af0a241d53fd
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-06-16 09:09:08 +0200

    digisim

--- os/main.c
+++ os/main.c
@@ -2,7 +2,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define BSIZE 81
+#define SWIDTH 3
+#define BWIDTH ((SWIDTH) * (SWIDTH))
+#define BSIZE ((BWIDTH) * (BWIDTH))
 
 static volatile int8_t TEST[BSIZE];
 
@@ -12,8 +14,7 @@ static void printgrid(const int8_t *grid) {
 }
 
 void *memcpy(void *dest, const void *src, size_t size) {
-  uint8_t *pdest = (uint8_t *)dest;
-  uint8_t *psrc = (uint8_t *)src;
+  uint8_t *pdest = (uint8_t *)dest, *psrc = (uint8_t *)src;
   for (size_t loops = (size / sizeof(uint32_t)); loops;
        --loops, pdest += sizeof(uint32_t), psrc += sizeof(uint32_t))
     *((uint32_t *)pdest) = *((uint32_t *)psrc);
@@ -23,32 +24,34 @@ void *memcpy(void *dest, const void *src, size_t size) {
 }
 
 static bool solve(int8_t *grid) {
-  // create local copy of grid
-  int8_t g[BSIZE], submarks[10];
-  memcpy(g, grid, BSIZE);
+  int8_t g[BSIZE], submarks[BWIDTH + 1];
+  memcpy(g, grid, sizeof(g));
   int8_t subindex, subcount;
   for (;;) {
     int8_t subs = 0;
     subindex = -1;
-    subcount = 10;
-    for (int8_t row = 0, i = 0, src = 0; row < 9; ++row, src += 9) {
-      int8_t topleft = row / 3 * 3 * 9;
-      int8_t rowmark[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, mark[10];
-      for (int8_t j = 0, rc = src; j < 9; ++j, ++rc)
+    subcount = BWIDTH + 1;
+    for (int8_t row = 0, i = 0, src = 0; row < BWIDTH; ++row, src += BWIDTH) {
+      int8_t topleft = row / SWIDTH * SWIDTH * BWIDTH;
+      int8_t rowmark[BWIDTH + 1], mark[BWIDTH + 1];
+      rowmark[0] = 0;
+      for (int8_t i = 1; i <= BWIDTH; ++i)
+        rowmark[i] = 1;
+      for (int8_t j = 0, rc = src; j < BWIDTH; ++j, ++rc)
         rowmark[g[rc]] = 0;
-      for (int8_t col = 0; col < 9; ++col, ++i) {
+      for (int8_t col = 0; col < BWIDTH; ++col, ++i) {
         if (g[i])
           continue;
         memcpy(mark, rowmark, sizeof(rowmark));
-        int8_t cc = col, c = topleft + (col / 3 * 3), count = 0, val;
-        for (int8_t j = 0; j < 9; ++j, cc += 9)
+        int8_t cc = col, c = topleft + (col / SWIDTH * SWIDTH), count = 0, val;
+        for (int8_t j = 0; j < BWIDTH; ++j, cc += BWIDTH)
           mark[g[cc]] = 0;
-        for (int8_t j = 0; j < 3; ++j, c += 6)
-          for (int8_t k = 0; k < 3; ++k, ++c)
+        for (int8_t j = 0; j < SWIDTH; ++j, c += (BWIDTH - SWIDTH))
+          for (int8_t k = 0; k < SWIDTH; ++k, ++c)
             mark[g[c]] = 0;
-        for (int8_t j = 1; j < 10; ++j)
+        for (int8_t j = 1; j <= BWIDTH; ++j)
           if (mark[j] != 0) {
-            val = mark[j];
+            val = j;
             ++count;
           }
         if (count == 0)
@@ -70,7 +73,7 @@ static bool solve(int8_t *grid) {
     memcpy(grid, g, sizeof(g));
     return true;
   }
-  for (int8_t i = 1; i <= 9; ++i) {
+  for (int8_t i = 1; i <= BWIDTH; ++i) {
     if (submarks[i] != 0) {
       g[subindex] = i;
       if (solve(g)) {
