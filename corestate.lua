b2a4067c45ef6892b8c85a00286684cd53478473
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-06-21 14:46:42 +0200

    digisim

--- corestate.lua
+++ corestate.lua
@@ -1,40 +1,40 @@
 #!/bin/luajit
 
 local positions = {
-	x1 = { 1, 1, 3, 33, changegroup = "register", alias = "ra" },
-	x2 = { 2, 1, 3, 33, changegroup = "register", alias = "sp" },
-	x3 = { 3, 1, 3, 33, changegroup = "register", alias = "gp" },
-	x4 = { 4, 1, 3, 33, changegroup = "register", alias = "tp" },
-	x5 = { 5, 1, 3, 33, changegroup = "register", alias = "t0" },
-	x6 = { 6, 1, 3, 33, changegroup = "register", alias = "t1" },
-	x7 = { 7, 1, 3, 33, changegroup = "register", alias = "t2" },
-	x8 = { 8, 1, 3, 33, changegroup = "register", alias = "s0" },
-	x9 = { 9, 1, 3, 33, changegroup = "register", alias = "s1" },
-	x10 = { 10, 1, 3, 33, changegroup = "register", alias = "a0" },
-	x11 = { 11, 1, 3, 33, changegroup = "register", alias = "a1" },
-	x12 = { 12, 1, 3, 33, changegroup = "register", alias = "a2" },
-	x13 = { 13, 1, 3, 33, changegroup = "register", alias = "a3" },
-	x14 = { 14, 1, 3, 33, changegroup = "register", alias = "a4" },
-	x15 = { 15, 1, 3, 33, changegroup = "register", alias = "a5" },
-	x16 = { 16, 1, 3, 33, changegroup = "register", alias = "a6" },
-	x17 = { 17, 1, 3, 33, changegroup = "register", alias = "a7" },
-	x18 = { 18, 1, 3, 33, changegroup = "register", alias = "s2" },
-	x19 = { 19, 1, 3, 33, changegroup = "register", alias = "s3" },
-	x20 = { 20, 1, 3, 33, changegroup = "register", alias = "s4" },
-	x21 = { 21, 1, 3, 33, changegroup = "register", alias = "s5" },
-	x22 = { 22, 1, 3, 33, changegroup = "register", alias = "s6" },
-	x23 = { 23, 1, 3, 33, changegroup = "register", alias = "s7" },
-	x24 = { 24, 1, 3, 33, changegroup = "register", alias = "s8" },
-	x25 = { 25, 1, 3, 33, changegroup = "register", alias = "s9" },
-	x26 = { 26, 1, 3, 33, changegroup = "register", alias = "s10" },
-	x27 = { 27, 1, 3, 33, changegroup = "register", alias = "s11" },
-	x28 = { 28, 1, 3, 33, changegroup = "register", alias = "t3" },
-	x29 = { 29, 1, 3, 33, changegroup = "register", alias = "t4" },
-	x30 = { 30, 1, 3, 33, changegroup = "register", alias = "t5" },
-	x31 = { 31, 1, 3, 33, changegroup = "register", alias = "t6" },
-	["[TIME]"] = { 1, 39, 5, 11, alias = "tick" },
-	["PC"] = { 3, 39, 5, 33, changegroup = "i", alias = "pc" },
-	["INSTR"] = { 4, 39, 5, 33, changegroup = "i", alias = "i" },
+	x1 = { 1, 1, 3, 8, changegroup = "register", alias = "ra", format = "hex" },
+	x2 = { 2, 1, 3, 8, changegroup = "register", alias = "sp", format = "hex" },
+	x3 = { 3, 1, 3, 8, changegroup = "register", alias = "gp", format = "hex" },
+	x4 = { 4, 1, 3, 8, changegroup = "register", alias = "tp", format = "hex" },
+	x5 = { 5, 1, 3, 8, changegroup = "register", alias = "t0", format = "hex" },
+	x6 = { 6, 1, 3, 8, changegroup = "register", alias = "t1", format = "hex" },
+	x7 = { 7, 1, 3, 8, changegroup = "register", alias = "t2", format = "hex" },
+	x8 = { 8, 1, 3, 8, changegroup = "register", alias = "s0", format = "hex" },
+	x9 = { 9, 1, 3, 8, changegroup = "register", alias = "s1", format = "hex" },
+	x10 = { 10, 1, 3, 8, changegroup = "register", alias = "a0", format = "hex" },
+	x11 = { 11, 1, 3, 8, changegroup = "register", alias = "a1", format = "hex" },
+	x12 = { 12, 1, 3, 8, changegroup = "register", alias = "a2", format = "hex" },
+	x13 = { 13, 1, 3, 8, changegroup = "register", alias = "a3", format = "hex" },
+	x14 = { 14, 1, 3, 8, changegroup = "register", alias = "a4", format = "hex" },
+	x15 = { 15, 1, 3, 8, changegroup = "register", alias = "a5", format = "hex" },
+	x16 = { 16, 1, 3, 8, changegroup = "register", alias = "a6", format = "hex" },
+	x17 = { 17, 1, 3, 8, changegroup = "register", alias = "a7", format = "hex" },
+	x18 = { 18, 1, 3, 8, changegroup = "register", alias = "s2", format = "hex" },
+	x19 = { 19, 1, 3, 8, changegroup = "register", alias = "s3", format = "hex" },
+	x20 = { 20, 1, 3, 8, changegroup = "register", alias = "s4", format = "hex" },
+	x21 = { 21, 1, 3, 8, changegroup = "register", alias = "s5", format = "hex" },
+	x22 = { 22, 1, 3, 8, changegroup = "register", alias = "s6", format = "hex" },
+	x23 = { 23, 1, 3, 8, changegroup = "register", alias = "s7", format = "hex" },
+	x24 = { 24, 1, 3, 8, changegroup = "register", alias = "s8", format = "hex" },
+	x25 = { 25, 1, 3, 8, changegroup = "register", alias = "s9", format = "hex" },
+	x26 = { 26, 1, 3, 8, changegroup = "register", alias = "s10", format = "hex" },
+	x27 = { 27, 1, 3, 8, changegroup = "register", alias = "s11", format = "hex" },
+	x28 = { 28, 1, 3, 8, changegroup = "register", alias = "t3", format = "hex" },
+	x29 = { 29, 1, 3, 8, changegroup = "register", alias = "t4", format = "hex" },
+	x30 = { 30, 1, 3, 8, changegroup = "register", alias = "t5", format = "hex" },
+	x31 = { 31, 1, 3, 8, changegroup = "register", alias = "t6", format = "hex" },
+	["[TIME]"] = { 1, 20, 5, 11, alias = "tick" },
+	["PC"] = { 3, 20, 5, 11, changegroup = "i", alias = "pc", format = "decimal" },
+	["INSTR"] = { 4, 20, 5, 11, changegroup = "i", alias = "i", format = "hex" },
 }
 
 function string:rpad(amt, char)
@@ -74,6 +74,18 @@ end
 ---@type table<string,table>
 local changegroups = {}
 
+local function parsenumber(value)
+	local v = bit.tobit(0)
+	for i = 0, value:len() do
+		local digit = value:sub(i, i)
+		v = bit.lshift(v, 1)
+		if digit ~= "0" then
+			v = bit.bor(v, bit.tobit(1))
+		end
+	end
+	return v
+end
+
 local function printat(pos, name, value)
 	if pos.alias ~= nil then
 		name = pos.alias
@@ -84,7 +96,14 @@ local function printat(pos, name, value)
 	end
 	value = value ~= nil and tostring(value) or ""
 	if value:len() > 0 then
-		value = value:lpad(pos[4])
+		if pos.format == "decimal" then
+			value = tostring(parsenumber(value)):lpad(pos[4])
+		elseif pos.format == "hex" then
+			local v = bit.tohex(parsenumber(value)):lpad(8, "0")
+			value = v:lpad(pos[4])
+		else
+			value = value:lpad(pos[4])
+		end
 	end
 	local str
 	if name:len() == 0 then
