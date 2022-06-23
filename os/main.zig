4778ad525ed3a2094a40a712fb2387a97b570d43
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-06-23 16:20:25 +0200

    digisim

--- os/main.zig
+++ os/main.zig
@@ -1,55 +1,94 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+const __heap: [32768]u8 = undefined;
+const allocator = &std.heap.FixedBufferAllocator.init(&__heap).allocator();
 
-const swidth  :u8= 3;
-const bwidth : u8 = swidth * swidth;
-const bsize : u8 = bwidth * bwidth;
+const Solver = struct {
+    root: *Node,
+    heap: *Allocator,
 
-var out : u8 = undefined;
+    const Node = struct {
+        top: *Node,
+        up: *Node,
+        down: *Node,
+        left: *Node,
+        right: *Node,
+    };
+
+    pub fn new(heap: *Allocator, square: i32) *Solver {
+        const alloc = try heap.create(Node);
+        _ = square;
+        _ = alloc;
+        return alloc;
+    }
+};
+
+const swidth: u8 = 3;
+const bwidth: u8 = swidth * swidth;
+const bsize: u8 = bwidth * bwidth;
+
+var out: u8 = undefined;
 var h_out: *volatile u8 = &out;
 
-pub fn output(grid:[]u8) void {
-    for (grid)|item| {
+pub fn output(grid: []u8) void {
+    for (grid) |item| {
         h_out.* = item;
     }
 }
 
-pub fn solve(grid:[]u8) bool {
-    var g : [bsize]u8 = undefined;
-    @memcpy(&g, grid.ptr,bsize);
+pub fn solve(grid: []u8) bool {
+    var g: [bsize]u8 = undefined;
+    @memcpy(&g, grid.ptr, bsize);
 
-    var subcell : u8 = bsize;
-    var submark : [bwidth+1]u8 = undefined;
-    var mark : [bwidth+1]u8 = undefined;
+    var subcell: u8 = bsize;
+    var submark: [bwidth + 1]u8 = undefined;
+    var mark: [bwidth + 1]u8 = undefined;
     mark[0] = 0;
-    @memset(mark[1..],1,bwidth);
+    @memset(mark[1..], 1, bwidth);
 
     while (true) {
-        var subs : u8 = 0;
-        var row : u8 = 0;
-        var cell : u8 = 0;
-        var rl : u8 = 0;
+        var subs: u8 = 0;
+        var row: u8 = 0;
+        var cell: u8 = 0;
+        var rl: u8 = 0;
 
-        while (row < bwidth) : ({row += 1;rl+=bwidth;}) {
-            var col : u8 = 0;
-            while (col < bwidth) : ({col += 1; cell += 1;}){
+        while (row < bwidth) : ({
+            row += 1;
+            rl += bwidth;
+        }) {
+            var col: u8 = 0;
+            while (col < bwidth) : ({
+                col += 1;
+                cell += 1;
+            }) {
                 if (g[cell] == 0) {
-                    @memset(&mark,1,bwidth+1);
+                    @memset(&mark, 1, bwidth + 1);
                     mark[0] = 0;
-                    var r : u8 = rl;
-                    var c : u8 = col;
-                    var s : u8 = ((swidth * bwidth) * (row / swidth)) + (swidth * (col / swidth));
+                    var r: u8 = rl;
+                    var c: u8 = col;
+                    var s: u8 = ((swidth * bwidth) * (row / swidth)) + (swidth * (col / swidth));
 
-                    var x : u8 = 0; while(x<swidth) : ({x += 1;s += bwidth - swidth;}){
-                        var y : u8 = 0; while(y<swidth) : ({y += 1; r += 1; c += bwidth; s += 1;}){
+                    var x: u8 = 0;
+                    while (x < swidth) : ({
+                        x += 1;
+                        s += bwidth - swidth;
+                    }) {
+                        var y: u8 = 0;
+                        while (y < swidth) : ({
+                            y += 1;
+                            r += 1;
+                            c += bwidth;
+                            s += 1;
+                        }) {
                             mark[g[r]] = 0;
                             mark[g[c]] = 0;
                             mark[g[s]] = 0;
                         }
                     }
 
-                    var val : u8 = undefined;
-                    var count : u8 = 0;
-                    for (mark[1..])|item, index|{
+                    var val: u8 = undefined;
+                    var count: u8 = 0;
+                    for (mark[1..]) |item, index| {
                         if (item != 0) {
                             val = @intCast(u8, index + 1);
                             count += 1;
@@ -62,7 +101,7 @@ pub fn solve(grid:[]u8) bool {
                         subs += 1;
                     } else {
                         subcell = cell;
-                        @memcpy(&submark,&mark,bwidth+1);
+                        @memcpy(&submark, &mark, bwidth + 1);
                     }
                 }
             }
@@ -71,7 +110,7 @@ pub fn solve(grid:[]u8) bool {
             break;
     }
     if (subcell < bsize) {
-        for (submark[1..])|item, index|{
+        for (submark[1..]) |item, index| {
             if (item != 0) {
                 g[subcell] = @intCast(u8, index + 1);
                 if (solve(g[0..])) {
@@ -81,8 +120,7 @@ pub fn solve(grid:[]u8) bool {
             }
         }
     }
-    if (subcell >= bsize)
-    {
+    if (subcell >= bsize) {
         @memcpy(grid.ptr, g[0..], bsize);
         return true;
     }
@@ -90,11 +128,7 @@ pub fn solve(grid:[]u8) bool {
 }
 
 pub export fn main() void {
-    var puzzle = [_]u8{0, 1, 3, 5, 0, 0, 4, 2, 0, 0, 8, 7, 0, 0, 4, 0, 0,
-                   0, 0, 0, 4, 0, 7, 9, 6, 0, 3, 0, 6, 2, 0, 4, 0, 5,
-                   0, 8, 0, 0, 0, 0, 5, 0, 1, 0, 2, 0, 3, 8, 0, 9, 1,
-                   0, 0, 0, 0, 0, 0, 9, 0, 0, 8, 0, 0, 7, 0, 0, 8, 1,
-                   5, 0, 0, 9, 8, 9, 1, 0, 0, 7, 2, 5, 0};
+    var puzzle = [_]u8{ 0, 1, 3, 5, 0, 0, 4, 2, 0, 0, 8, 7, 0, 0, 4, 0, 0, 0, 0, 0, 4, 0, 7, 9, 6, 0, 3, 0, 6, 2, 0, 4, 0, 5, 0, 8, 0, 0, 0, 0, 5, 0, 1, 0, 2, 0, 3, 8, 0, 9, 1, 0, 0, 0, 0, 0, 0, 9, 0, 0, 8, 0, 0, 7, 0, 0, 8, 1, 5, 0, 0, 9, 8, 9, 1, 0, 0, 7, 2, 5, 0 };
     if (solve(puzzle[0..])) {
         output(puzzle[0..]);
     }
