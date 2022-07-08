ef4e97863ee555f18392446b1a57a41493f92b20
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-07-08 14:20:21 +0200

    digisim

--- zigsim/digisim.zig
+++ zigsim/digisim.zig
@@ -1,6 +1,8 @@
 const t = @import("types.zig");
 const Component = @import("sim/component.zig").Component;
+const Port = @import("sim/port.zig").Port;
 const stringIntern = @import("stringIntern.zig");
+const std = @import("std");
 
 pub const Digisim = struct {
     allocator: t.Allocator,
@@ -30,12 +32,58 @@ pub const Digisim = struct {
         return self.id;
     }
 
+    pub fn findComponent(self: *@This(), id: t.Id) ?*Component {
+        return self.components.getPtr(id);
+    }
+
+    pub fn findComponentByName(self: *@This(), digisim: Digisim, name: []const u8) ?*Component {
+        if (digisim.strings.get(name)) |interned_name| {
+            var i = self.ports.iterator();
+            while (i.next()) |entry| {
+                if (entry.value_ptr.name.ptr == interned_name.ptr) return entry.value_ptr;
+            }
+        }
+        return null;
+    }
+
     pub fn addComponent(self: *@This(), name: []const u8) !t.Id {
+        if (name.len == 0) return error.DigisimInvalidComponentName;
+        if (std.mem.indexOf(u8, name, ".")) |_| return error.DigisimInvalidComponentName;
         const interned_name = try self.strings.ref(name);
         errdefer self.strings.unref(interned_name);
+        var i = self.components.iterator();
+        while (i.next()) |entry| {
+            if (entry.value_ptr.name.ptr == interned_name.ptr) {
+                return error.DigisimDuplicateComponentName;
+            }
+        }
         var comp = try Component.init(self, interned_name);
         errdefer comp.deinit(self);
         try self.components.put(comp.id, comp);
         return comp.id;
+    }
+
+    pub fn getPort(self: *@This(), name: []const u8) !?*Port {
+        if (std.mem.lastIndexOf(u8, name, ".")) |index| {
+            const port_name = name[index + 1 ..];
+            if (port_name.len == 0) return error.DigisimInvalidPortName;
+
+            var name_chain = name[0..index];
+            if (name_chain.len == 0) return error.DigisimInvalidComponentName;
+            while (name_chain.len > 0) {
+                var pname: []const u8 = undefined;
+                if (std.mem.indexOf(u8, name_chain, ".")) |idx| {
+                    pname = name_chain[0..idx];
+                    name_chain = name_chain[idx + 1 ..];
+                } else {
+                    pname = name_chain;
+                    name_chain = name_chain[0..0];
+                }
+                if (pname.len == 0) return error.DigisimInvalidComponentName;
+                if (self.strings.get(pname)) |n| {
+                    pname = n;
+                }
+            }
+        } else return null;
     }
 };
--- zigsim/sim/component.zig
+++ zigsim/sim/component.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const t = @import("../types.zig");
 const Port = @import("port.zig").Port;
 const Digisim = @import("../digisim.zig").Digisim;
@@ -25,9 +26,29 @@ pub const Component = struct {
         self.ports.deinit();
     }
 
+    pub fn findPort(self: *@This(), id: t.Id) ?*Port {
+        return self.ports.getPtr(id);
+    }
+
+    pub fn findPortByName(self: *@This(), digisim: Digisim, name: []const u8) ?*Port {
+        if (digisim.strings.get(name)) |interned_name| {
+            var i = self.ports.iterator();
+            while (i.next()) |entry| {
+                if (entry.value_ptr.name.ptr == interned_name.ptr) return entry.value_ptr;
+            }
+        }
+        return null;
+    }
+
     pub fn addPort(self: *@This(), digisim: *Digisim, name: []const u8) !t.Id {
+        if (name.len == 0) return error.DigisimInvalidPortName;
+        if (std.mem.indexOf(u8, name, ".")) |_| return error.DigisimInvalidPortName;
         const interned_name = digisim.strings.ref(name);
         errdefer digisim.strings.unref(interned_name);
+        var i = self.ports.iterator();
+        while (i.next()) |entry| {
+            if (entry.value_ptr.name.ptr == interned_name.ptr) return error.DigisimDuplicatePortName;
+        }
         var p = Port.init(digisim, interned_name);
         errdefer p.deinit();
         try self.ports.put(p.id, p);
