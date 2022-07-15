921e876a7a34708f1d68f888c7c45cc3891aec90
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-07-15 08:25:40 +0200

    x

--- build.zig
+++ build.zig
@@ -20,7 +20,7 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest("src/main.zig");
+    const exe_tests = b.addTest("zigsim/main.zig");
     exe_tests.setTarget(target);
     exe_tests.setBuildMode(mode);
 
