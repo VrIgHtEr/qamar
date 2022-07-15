814985c4021d357b086edeba1885ba5e5e62be15
Cedric Mamo <vrighter@gmail.com>
2022-07-15 14:12:40 +0200

    updated string interning

--- zigsim/stringIntern.zig
+++ zigsim/stringIntern.zig
@@ -1,51 +1,44 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-const Entry = struct {
-    refcount: usize = 0,
-    value: []const u8,
-
-    pub fn init(value: []const u8) @This() {
-        return @This(){ .value = value };
-    }
-};
-
 pub const StringIntern = struct {
     allocator: Allocator,
-    strings: std.StringHashMap(Entry),
+    strings: std.StringHashMap(usize),
 
     pub fn init(allocator: Allocator) @This() {
-        var ret: @This() = .{ .allocator = allocator, .strings = std.StringHashMap(Entry).init(allocator) };
+        var ret: @This() = .{ .allocator = allocator, .strings = std.StringHashMap(usize).init(allocator) };
         return ret;
     }
 
     pub fn get(self: *@This(), value: []const u8) ?[]const u8 {
-        if (self.strings.getPtr(value)) |e| {
-            return e.value;
+        if (self.strings.getKeyPtr(value)) |e| {
+            return e.*;
         }
         return null;
     }
 
     pub fn ref(self: *@This(), value: []const u8) ![]const u8 {
-        var entry: *Entry = undefined;
-        if (self.strings.getPtr(value)) |e| {
-            entry = e;
+        var entry: *usize = undefined;
+        var key: []const u8 = undefined;
+        if (self.strings.getEntry(value)) |e| {
+            entry = e.value_ptr;
+            key = e.key_ptr.*;
         } else {
-            var key = try self.allocator.alloc(u8, value.len);
-            std.mem.copy(u8, key, value);
-            var t = Entry.init(key);
-            try self.strings.put(key, t);
-            entry = self.strings.getPtr(value) orelse unreachable;
+            var k = try self.allocator.alloc(u8, value.len);
+            std.mem.copy(u8, k, value);
+            try self.strings.put(k, 0);
+            entry = self.strings.getPtr(k) orelse unreachable;
+            key = k;
         }
-        entry.refcount += 1;
-        return entry.value;
+        entry.* += 1;
+        return key;
     }
 
     pub fn unref(self: *@This(), value: []const u8) void {
-        if (self.strings.getPtr(value)) |e| {
-            e.refcount -= 1;
-            if (e.refcount == 0) {
-                const key = e.value;
+        if (self.strings.getEntry(value)) |e| {
+            e.value_ptr.* -= 1;
+            if (e.value_ptr.* == 0) {
+                var key = e.key_ptr.*;
                 _ = self.strings.remove(key);
                 self.allocator.free(key);
             }
