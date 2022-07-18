d75736715343eeb6e5f6f7174f652b348f61cb40
Cedric Mamo <vrighter@gmail.com>
2022-07-19 01:28:51 +0200

    digisim

--- zigsim/signal.zig
+++ zigsim/signal.zig
@@ -10,24 +10,24 @@ pub const Signal = enum(u8) {
     weakhigh = 7,
 
     pub fn resolve(self: @This(), other: @This()) @This() {
-        return restable[@enumToInt(self) * @typeInfo(@This()).Enum.fields.len + @enumToInt(other)];
+        return resolvetable[@enumToInt(self) * @typeInfo(@This()).Enum.fields.len + @enumToInt(other)];
     }
 
     pub fn collapse(self: @This()) Signal {
-        if (self == Signal.high or self == Signal.weakhigh) return Signal.high;
-        return Signal.low;
+        return collapsetable[@enumToInt(self)];
     }
 
     pub fn tovcd(self: @This()) Signal {
-        if (self == Signal.low or self == Signal.weaklow) return Signal.low;
-        if (self == Signal.high or self == Signal.weakhigh) return Signal.high;
-        if (self == Signal.z) return Signal.z;
-        return Signal.unknown;
+        return tovcdtable[@enumToInt(self)];
     }
 };
 
-const restable = [_]Signal{ Signal.uninitialized, Signal.unknown, Signal.low, Signal.high, Signal.z, Signal.weak, Signal.weaklow, Signal.weakhigh, Signal.unknown, Signal.unknown, Signal.unknown, Signal.unknown, Signal.unknown, Signal.unknown, Signal.unknown, Signal.unknown, Signal.low, Signal.unknown, Signal.low, Signal.unknown, Signal.low, Signal.low, Signal.low, Signal.low, Signal.high, Signal.unknown, Signal.unknown, Signal.high, Signal.high, Signal.high, Signal.high, Signal.high, Signal.z, Signal.unknown, Signal.low, Signal.high, Signal.z, Signal.weak, Signal.weaklow, Signal.weakhigh, Signal.weak, Signal.unknown, Signal.low, Signal.high, Signal.weak, Signal.weak, Signal.weak, Signal.weak, Signal.weaklow, Signal.unknown, Signal.low, Signal.high, Signal.weaklow, Signal.weak, Signal.weaklow, Signal.weak, Signal.weakhigh, Signal.unknown, Signal.low, Signal.high, Signal.weakhigh, Signal.weak, Signal.weak, Signal.weakhigh };
+const resolvetable = [_]Signal{ Signal.uninitialized, Signal.unknown, Signal.low, Signal.high, Signal.z, Signal.weak, Signal.weaklow, Signal.weakhigh, Signal.unknown, Signal.unknown, Signal.unknown, Signal.unknown, Signal.unknown, Signal.unknown, Signal.unknown, Signal.unknown, Signal.low, Signal.unknown, Signal.low, Signal.unknown, Signal.low, Signal.low, Signal.low, Signal.low, Signal.high, Signal.unknown, Signal.unknown, Signal.high, Signal.high, Signal.high, Signal.high, Signal.high, Signal.z, Signal.unknown, Signal.low, Signal.high, Signal.z, Signal.weak, Signal.weaklow, Signal.weakhigh, Signal.weak, Signal.unknown, Signal.low, Signal.high, Signal.weak, Signal.weak, Signal.weak, Signal.weak, Signal.weaklow, Signal.unknown, Signal.low, Signal.high, Signal.weaklow, Signal.weak, Signal.weaklow, Signal.weak, Signal.weakhigh, Signal.unknown, Signal.low, Signal.high, Signal.weakhigh, Signal.weak, Signal.weak, Signal.weakhigh };
+const collapsetable = [_]Signal{ Signal.low, Signal.low, Signal.low, Signal.high, Signal.low, Signal.low, Signal.low, Signal.high };
+const tovcdtable = [_]Signal{ Signal.unknown, Signal.unknown, Signal.low, Signal.high, Signal.z, Signal.unknown, Signal.low, Signal.high };
 
 comptime {
-    if (restable.len != std.math.pow(usize, @typeInfo(Signal).Enum.fields.len, 2)) unreachable;
+    if (resolvetable.len != std.math.pow(usize, @typeInfo(Signal).Enum.fields.len, 2)) unreachable;
+    if (collapsetable.len != @typeInfo(Signal).Enum.fields.len) unreachable;
+    if (tovcdtable.len != @typeInfo(Signal).Enum.fields.len) unreachable;
 }
