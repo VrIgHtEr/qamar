23751643dfa0e3fa80b900dd4a79ccb3c38b5a59
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-07-19 14:38:31 +0200

    digisim

--- zigsim/lua/init.lua
+++ zigsim/lua/init.lua
@@ -1,1 +1,36 @@
---print("Hello world!!!")
+---@class zigcomp
+---@field id number
+local Component = {}
+local Component_MT = {
+	__index = Component,
+}
+
+---@param id number
+---@return zigcomp
+function Component.new(id)
+	if type(id) ~= "number" or id < 0 then
+		error("invalid id")
+	end
+	return setmetatable({ id = math.floor(id) }, Component_MT)
+end
+
+local base_env = { math = math, pairs = pairs, ipairs = ipairs, getfenv = getfenv, tostring = tostring, print = print }
+local env_mt = { __index = base_env }
+
+local function create_env(id)
+	local env = setmetatable({}, env_mt)
+	function env.printid()
+		print(id)
+	end
+	return env
+end
+
+function Component:construct(constructor, ...)
+	local c = function(...)
+		setfenv(constructor, create_env(self.id))
+		return constructor(...)
+	end
+	return c(...)
+end
+
+return Component
